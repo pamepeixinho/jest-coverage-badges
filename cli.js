5adbb2b0522cceeb57e0410b4e107a1edb8c84b0
Michael Rodov <michael.rodov@imperva.com>
2018-12-20 13:14:47 +0200

    - added parametrized input and output

--- cli.js
+++ cli.js
@@ -1,55 +1,87 @@
 #!/usr/bin/env node
-
+var mkdirp = require('mkdirp')
 const { get } = require('https')
 const { readFile, writeFile } = require('fs')
 
+const outputPath = _findArgument('output', './coverage')
+const inputPath = _findArgument('input', './coverage/coverage-summary.json')
+
 const getColour = coverage => {
   if (coverage < 80) {
-    return 'red';
+    return 'red'
   }
 
   if (coverage < 90) {
-    return 'yellow';
+    return 'yellow'
   }
 
-  return 'brightgreen';
+  return 'brightgreen'
 }
 
-const reportKeys = ['lines', 'statements', 'functions', 'branches'];
+const reportKeys = ['lines', 'statements', 'functions', 'branches']
 
 const getBadge = (report, key) => {
   if (!(report && report.total && report.total[key])) {
-    throw new Error('malformed coverage report');
+    throw new Error('malformed coverage report')
   }
 
-  const coverage = report.total[key].pct;
-  const colour = getColour(coverage);
+  const coverage = report.total[key].pct
+  const colour = getColour(coverage)
 
-  return `https://img.shields.io/badge/Coverage-${coverage}${encodeURI('%')}-${colour}.svg`;
+  return `https://img.shields.io/badge/Coverage-${coverage}${encodeURI('%')}-${colour}.svg`
 }
 
 const download = (url, cb) => {
   get(url, res => {
-    let file = '';
-    res.on('data', chunk => (file += chunk));
-    res.on('end', () => cb(null, file));
-  }).on('error', err => cb(err));
-};
-
+    let file = ''
+    res.on('data', chunk => (file += chunk))
+    res.on('end', () => cb(null, file))
+  }).on('error', err => cb(err))
+}
 
 const getBadgeByKey = (report) => (key) => {
-  const url = getBadge(report, key);
+  const url = getBadge(report, key)
 
   download(url, (err, res) => {
     if (err) throw err
-    writeFile(`./coverage/badge-${key}.svg`, res, 'utf8', err => {
-      if (err) throw err
-    });
-  });
-};
+    mkdirp(outputPath, function (err) {
+      if (err) {
+        console.error(`Could not create output directory ${err}`)
+      } else {
+        writeFile(`${outputPath}/badge-${key}.svg`, res, 'utf8', err => {
+          if (err) throw err
+        })
+      }
+    })
+  })
+}
 
-readFile('./coverage/coverage-summary.json', 'utf8', (err, res) => {
+readFile(`${inputPath}`, 'utf8', (err, res) => {
   if (err) throw err
-  const report = JSON.parse(res);
-  reportKeys.forEach(getBadgeByKey(report));
-});
+  const report = JSON.parse(res)
+  reportKeys.forEach(getBadgeByKey(report))
+})
+
+/**
+ * Will lookup the argument in the cli arguments list and will return a value passed as CLI arg (if found)
+ * Otherwise will return default value passed
+ * @param argName - name of hte argument to look for
+ * @param defaultOutput - default value to return if could not find argument in cli command
+ * @private
+ */
+function _findArgument (argName, defaultOutput) {
+  if (!argName) {
+    return defaultOutput
+  }
+
+  const index = process.argv.findIndex(a => a.match(argName))
+  if (index < 0) {
+    return defaultOutput
+  } else {
+    try {
+      return process.argv[index + 1]
+    } catch (e) {
+      return defaultOutput
+    }
+  }
+}
