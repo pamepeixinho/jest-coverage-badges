c066ea05675d6f97549a78ddc434386d3b63c9b0
Michael Rodov <michael.rodov@imperva.com>
2018-12-23 12:00:06 +0200

    - re-added semicolons

--- cli.js
+++ cli.js
@@ -1,55 +1,55 @@
-#!/usr/bin/env node
-const mkdirp = require('mkdirp')
-const { get } = require('https')
-const { readFile, writeFile } = require('fs')
-const outputPath = _findArgument('output', './coverage')
-const inputPath = _findArgument('input', './coverage/coverage-summary.json')
+/* eslint-disable semi */
+const mkdirp = require('mkdirp');
+const { get } = require('https');
+const { readFile, writeFile } = require('fs');
+
+const outputPath = _findArgument('output', './coverage');
+const inputPath = _findArgument('input', './coverage/coverage-summary.json');
 
 const getColour = coverage => {
   if (coverage < 80) {
-    return 'red'
+    return 'red';
   }
 
   if (coverage < 90) {
-    return 'yellow'
+    return 'yellow';
   }
 
   return 'brightgreen';
 };
 
-const reportKeys = ['lines', 'statements', 'functions', 'branches']
+const reportKeys = ['lines', 'statements', 'functions', 'branches'];
 
 const getBadge = (report, key) => {
   if (!(report && report.total && report.total[key])) {
-    throw new Error('malformed coverage report')
+    throw new Error('malformed coverage report');
   }
 
-  const coverage = report.total[key].pct
-  const colour = getColour(coverage)
+  const coverage = report.total[key].pct;
+  const colour = getColour(coverage);
 
-  return `https://img.shields.io/badge/Coverage-${coverage}${encodeURI('%')}-${colour}.svg`
+  return `https://img.shields.io/badge/Coverage-${coverage}${encodeURI('%')}-${colour}.svg`;
 }
 
 const download = (url, cb) => {
   get(url, res => {
-    let file = ''
-    res.on('data', chunk => (file += chunk))
-    res.on('end', () => cb(null, file))
-  }).on('error', err => cb(err))
+    let file = '';
+    res.on('data', chunk => (file += chunk));
+    res.on('end', () => cb(null, file));
+  }).on('error', err => cb(err));
 }
 
-
 const getBadgeByKey = (report) => (key) => {
-  const url = getBadge(report, key)
+  const url = getBadge(report, key);
 
   download(url, (err, res) => {
     if (err) throw err
     mkdirp(outputPath, function (err) {
       if (err) {
-        console.error(`Could not create output directory ${err}`)
+        console.error(`Could not create output directory ${err}`);
       } else {
         writeFile(`${outputPath}/badge-${key}.svg`, res, 'utf8', err => {
-          if (err) throw err
+          if (err) throw err;
         })
       }
     })
@@ -57,9 +57,11 @@ const getBadgeByKey = (report) => (key) => {
 }
 
 readFile(`${inputPath}`, 'utf8', (err, res) => {
-  if (err) throw err
-  const report = JSON.parse(res)
-  reportKeys.forEach(getBadgeByKey(report))
+  if (err) {
+    throw err
+  }
+  const report = JSON.parse(res);
+  reportKeys.forEach(getBadgeByKey(report));
 })
 
 /**
@@ -71,19 +73,17 @@ readFile(`${inputPath}`, 'utf8', (err, res) => {
  */
 function _findArgument (argName, defaultOutput) {
   if (!argName) {
-    return defaultOutput
+    return defaultOutput;
   }
 
   const index = process.argv.findIndex(a => a.match(argName))
   if (index < 0) {
-    return defaultOutput
+    return defaultOutput;
   } else {
     try {
-      return process.argv[index + 1]
+      return process.argv[index + 1];
     } catch (e) {
-      return defaultOutput
+      return defaultOutput;
     }
   }
 }
-
-
